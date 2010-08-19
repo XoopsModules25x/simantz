d2268094dac7adceb3cb0d124ed9ddb22a650e3e
skysong1988 <skysong1988@3f281f88-36d0-3f56-a6f9-8d4f7be19bbc>
2010-08-19 12:17:26 +0000

    approval stablize

--- simantz/trunk/modules/approval/admin/index.php
+++ simantz/trunk/modules/approval/admin/index.php
@@ -11,10 +11,8 @@ echo <<< EOF
 <tbody>
 <tr>
 <th>Description</th><th>Link</th></tr>
-<tr><td><b>Windows</b><br>Add/Edit Windows/Report in this system(You shall not simply change data here unless you know what you are doing).</td><td><A href="window.php">Add/Edit Window</a></td></tr>
-<tr><td><b>Permission</b><br>Manage user groups permission from accessing particular function(You shall create group and assign group to access SimTrain Module before proceed to this step).</td><td><A href="permission.php"> Add / Edit Group Permission</a></td></tr>
 <tr><td><b>Extra Setting</b><br></td><td><A href="changesetting.php">Extra Setting</a></td></tr>
-<tr><td><b>Activity Summary Report</b><br>Summary For Save,Update and Delete</td><td><A href="activitysummary.php">Activity Summary Report</a></td></tr>
+
 </tbody>
 </table>
 EOF;
