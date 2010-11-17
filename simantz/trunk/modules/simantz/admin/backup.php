3d3cfbb8fc1cf0389934db180f1127a9fa19c014
kstan79 <kstan79@3f281f88-36d0-3f56-a6f9-8d4f7be19bbc>
2010-11-17 14:03:19 +0000

    fix backup file function, enforce encryption of database.

--- simantz/trunk/modules/simantz/admin/backup.php
+++ simantz/trunk/modules/simantz/admin/backup.php
@@ -1,6 +1,6 @@
 <?php
 include_once "admin_header.php" ;
-
+include "../setting.php";
 #include 'config.php';
 #include 'opendb.php';
 $dbname=XOOPS_DB_NAME;
@@ -13,31 +13,36 @@ $dbhost=XOOPS_DB_HOST;
 //$random = (rand()%999);
 
 if(PHP_OS=='WINNT'){
-$backupFile = "backup\\backup.sql";
+$backupFile = "..\\".$uploadpath."\\backup.sql";
 $mysqlaction="d:\wamp\bin\mysql\mysql5.0.51a\bin\mysqldump.exe";
 system("del backup\\backup*;");
 system("echo SET FOREIGN_KEY_CHECKS=0; > $backupFile");
-system("$mysqlaction --routines --no-create-db -h $dbhost -u $dbuser -p$dbpass $dbname >> $backupFile");
-system("zip -P '$dbpass' $backupFile.zip $backupFile > backup\\log.log");
+system("echo SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO'; >> $backupFile");
+system("$mysqlaction --routines --no-create-db -h $dbhost -u $dbuser -p$dbpass --add-drop-table --routines  $dbname >> $backupFile");
+system("zip -q -P '$dbpass' $backupFile.zip $backupFile > backup\\log.log");
 }
 else{
-$backupFile = "backup/backup.sql";
-$mysqlaction="/usr/bin/mysqldump";
-system("rm -rf backup/*;");
-system("echo 'SET FOREIGN_KEY_CHECKS=0;' > $backupFile");
-system("$mysqlaction --routines --no-create-db -h $dbhost -u $dbuser -p$dbpass $dbname >> $backupFile");
-system("zip -P '$dbpass' $backupFile.zip $backupFile > backup/log.log");
-}
+$backupFile = "../$uploadpath/backup.sql";
 
+//$mysqlaction="/usr/bin/mysqldump";
+$mysqlaction="mysqldump";
+system("rm -rf ../$uploadpath/*.sql;");
+
+system("echo 'SET FOREIGN_KEY_CHECKS=0;' > $backupFile");
+system("echo 'SET SQL_MODE=\"NO_AUTO_VALUE_ON_ZERO\";' >> $backupFile");
+system("$mysqlaction --routines --no-create-db -h $dbhost -u $dbuser -p$dbpass --add-drop-table --routines  $dbname >> $backupFile");
+system("zip -q -P  '$dbpass' $backupFile.zip $backupFile");
+
+}
+//echo  "???".filesize("$backupFile.zip");
+//die;
 header('Content-Type: application/force-download');
-header('Content-Type: application/octet-stream');
+header('Content-Type: application/zip');
 header('Content-Type: application/download');
 header('Content-Description: File Transfer');
 header('Content-Length: ' . filesize("$backupFile.zip"));
 header("Content-disposition: attachment; filename=$backupFile".".zip");
 
-//header("Content-length: " . strlen($zipcontents) );
-//readfile($backupFile);
 
 $fp = fopen("$backupFile.zip", "r");
 while (!feof($fp))
@@ -47,5 +52,3 @@ while (!feof($fp))
 } 
 fclose($fp); 
 
-
-?>
