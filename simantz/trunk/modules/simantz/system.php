bb32deed20ea89651cbcd909c1c04715d827c84a
kstan79 <kstan79@3f281f88-36d0-3f56-a6f9-8d4f7be19bbc>
2010-11-30 20:08:33 +0000

    add userid 1 access all page

--- simantz/trunk/modules/simantz/system.php
+++ simantz/trunk/modules/simantz/system.php
@@ -140,7 +140,7 @@
  
 
 
-	if($menuname == "" && $userid > 0)
+	if($menuname == "" && $userid > 1)
 		 redirect_header("index.php",$pausetime,
 			"<b style='color:red'>You don't have permission to access this page, back to home.</b> ");
         
