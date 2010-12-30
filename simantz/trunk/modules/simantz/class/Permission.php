6dbfc4e83c26f18bbbf56487b1cbddb936a0e6ae
kstan79 <kstan79@gmail.com>
2010-12-30 09:12:47 +0000

    Make permission table view become more user friendly

--- simantz/trunk/modules/simantz/class/Permission.php
+++ simantz/trunk/modules/simantz/class/Permission.php
@@ -40,7 +40,7 @@ public function Permission (){
 
 
 	$this->log->showLog(4,"Accessing showcontrolheader with groupid: $groupid");
-	
+
 	$sql="SELECT g.groupid, g.name from sim_groups g
 		 order by g.name";
 	$selectgroup="<SELECT name='groupid' onchange='action.click();'>";
@@ -54,7 +54,7 @@ public function Permission (){
 			$select_it="SELECTED='SELECTED'";
 		else
 			$select_it="";
-	
+
 		$selectgroup=$selectgroup."<option $select_it value='$linegroupid'>$groupname</option>";
 
 	}
@@ -62,13 +62,13 @@ public function Permission (){
 	$selectgroup=$selectgroup."</SELECT>";
 	$this->log->showLog(4,"With SQL: $sql");
 
-        $sql="SELECT m.mid,m.name from sim_modules m where m.weight>0
-		 order by m.name ";
+        $sql="SELECT m.mid,m.name from sim_modules m
+		 order by m.name";
 	$selectmodule="<SELECT name='findmodule_id' onchange='action.click();'>";
 	$i=0;
 	$query=$this->xoopsDB->query($sql);
 	$select_it="";
-        
+
 	while($row=$this->xoopsDB->fetchArray($query)){
 		 $linemid=$row['mid'];
 		 $mname=$row['name'];
@@ -86,17 +86,17 @@ public function Permission (){
 
 	echo <<<EOF
 <A href='index.php'>Back To This Module Administration Menu</A>
-	 <TABLE class="searchformblock">
+	 <TABLE>
 		<TBODY>
-		<tr><td colspan='5' style='text-align: center' class='tdListRightTitle'>Search Groups</td></tr>
-		<tr><FORM method='GET' action='permission.php'>
+		<TR><TH colspan='5' style='text-align: center'>Search Groups</TH></TR>
+		<TR><FORM method='GET' action='permission.php'>
 			<TD class="head">Group</TD>
 			<TD class="odd">$selectgroup <A href='group.php'>Add/Edit Groups</A></TD>
 			<TD class="head">Module</TD><TD class="odd">$selectmodule</TD>
 
                     <TD class="even"><INPUT type='submit' name="action" value="search"></TD>
 			</FORM>
-		</tr>
+		</TR>
 		</TBODY>
 	 </TABLE>
 <br>
@@ -107,24 +107,24 @@ EOF;
 
   public function showPermissionTable($groupid,$mid){
 	$this->log->showLog(4,"Accessing showcontrolheader with groupid: $groupid");
-	
+
 
 
 //show table header
 echo <<< EOF
 <FORM method='POST' action='permission.php' name='frmUpdatePermission'>
-	 <TABLE class='searchformblock'>
+	 <TABLE>
 		<TBODY>
-		<tr><td colspan='10' style='text-align: center' class='tdListRightTitle'>Add/Edit Group Permission
+		<TR><TH colspan='10' style='text-align: center'>Add/Edit Group Permission
                         <INPUT TYPE='submit' value='save' name='submit'>
                         <INPUT TYPE='hidden' value='$this->groupid' name='groupid'>
-                        </td></tr>
+                        </TH></TR>
 		<TR>
 			<Th>No</Th>
 			<Th>Function Name</Th>
 			<Th>Module Name</Th>
 			<Th>File Name</Th>
-			<Th>Parent</Th>
+			<Th>Description</Th>
 			<Th>Seq</Th>
                         <Th>R<br/><input name="readperm" type="checkbox"  onclick=selectall(this.checked)><br>
 				</Th>
@@ -149,7 +149,7 @@ $this->showPermissionTableDetail($groupid,$mid,0,$level);
 
 
   public function showPermissionTableDetail($groupid,$mid,$window_id,$level){
-      
+
    $sql="SELECT w.window_id,w.window_name,w.filename,w.parentwindows_id,w.seqno,w.windowsetting,
                 w2.window_name as parentname,m.name as module_name,
                 (SELECT permissionsetting FROM
@@ -165,10 +165,10 @@ $this->showPermissionTableDetail($groupid,$mid,0,$level);
                LEFT JOIN sim_window w2 on w2.window_id=w.parentwindows_id
                WHERE w.window_id>0 and w.mid=$mid and w.parentwindows_id=$window_id
                     order by m.name, w2.window_name, w.seqno";
-  
+
 	$this->log->showLog(4,"With SQL: $sql");
 
-        
+
         $i=0;
 
 
@@ -183,7 +183,7 @@ $this->showPermissionTableDetail($groupid,$mid,0,$level);
                     $window_name= "&nbsp;&nbsp;&nbsp;&nbsp;$window_name";
 		$window_id=$row['window_id'];
 		$parentname=$row['parentname'];
-		
+		$description=str_replace("\n", "<br/>", $row['description']);
                 $iswriteperm=$row['iswriteperm'];
                 $seqno=$row['seqno'];
                 $module_name=$row['module_name'];
@@ -195,7 +195,7 @@ $this->showPermissionTableDetail($groupid,$mid,0,$level);
 	$checked="checked";
 	else
 	$checked="";
-        
+
         if($iswriteperm>0)
 	$writechecked="checked";
 	else
@@ -213,13 +213,13 @@ $this->showPermissionTableDetail($groupid,$mid,0,$level);
 			<TD class="$rowtype">$window_name</TD>
 			<TD class="$rowtype">$module_name</TD>
 			<TD class="$rowtype">$filename</TD>
-			<TD class="$rowtype">$parentname</TD>
+			<TD class="$rowtype">$description</TD>
 			<TD class="$rowtype">$seqno</TD>
 			<TD class="$rowtype"><INPUT type='checkbox' name="lineallow[$i]" $checked></TD>
 			<TD class="$rowtype"><INPUT type='checkbox' name="lineallowwrite[$i]" $writechecked>
 					<input type='hidden' name='linewindow_id[$i]' value='$window_id'></TD>
-			<TD class="$rowtype"><INPUT name="linevaliduntil[$i]" value="$validuntil"></TD>
-			<TD class="$rowtype"><INPUT name="linepermissionsetting[$i]"  value="$permissionsetting" title="option:description"></TD>
+			<TD class="$rowtype"><INPUT name="linevaliduntil[$i]" value="$validuntil" size="10"></TD>
+			<TD class="$rowtype"><INPUT name="linepermissionsetting[$i]"  value="$permissionsetting" size="60" title="option:description"></TD>
 		</TR>
 
 EOF;
@@ -230,7 +230,7 @@ EOF;
 
   public function updatePermission(){
 	$timestamp= date("y/m/d H:i:s", time()) ;
- 
+
 	$this->log->showLog(4,"Accessing updatePermission with groupid=$this->groupid");
 	 $sqldel="DELETE FROM sim_permission where groupid=$this->groupid and
                         window_id in (select w.window_id from sim_window w where mid=$this->findmodule_id)";
@@ -253,7 +253,7 @@ EOF;
                                 ($winid,$this->groupid,'$timestamp',$this->updatedby,'$timestamp',
                             $this->createdby,'$permissionsetting','$validuntil',
                                 $allowwritevalue)";
-                        
+
 			$this->log->showLog(4,"With SQL2: $sqlinsert");
 			$this->xoopsDB->query($sqlinsert);
 		}
@@ -283,8 +283,8 @@ function showMenu($parentwindows_id,$level,$uid,$module_id){
                     and w.mid=$module_id
                     and w.parentwindows_id=$parentwindows_id and u.uid=$uid and
                     w.isactive=1 and w.window_id>0
-                  and ( gsp.validuntil = '0000-00-00' OR gsp.validuntil >= '$currentdate') order by w.seqno, w.window_name ASC";
-   
+                  and ( gsp.validuntil = '0000-00-00' OR gsp.validuntil >= '$currentdate') order by w.seqno";
+
   $level++;
  $query=$this->xoopsDB->query($sql);
     while($row=$this->xoopsDB->fetchArray($query)){
@@ -299,8 +299,8 @@ function showMenu($parentwindows_id,$level,$uid,$module_id){
             $cssclass="class='sub menulink'";
             $linkname="";
         }
-       
-       
+
+
         $output .= "<li><a $linkname $cssclass>".$row['window_name']."</a><ul>";
 
 
@@ -318,7 +318,7 @@ function showMenu($parentwindows_id,$level,$uid,$module_id){
         $sql="select distinct(w.window_id) as window_id,w.helpurl,
 		w.window_name, gsp.iswriteperm, gsp.permissionsetting ,w.seqno,w.filename,
             w.windowsetting,w.mid
-		from sim_groups g 
+		from sim_groups g
 		inner join sim_groups_users_link gul on g.groupid=gul.groupid
 		inner join sim_users u on gul.uid=u.uid
 		inner join sim_group_permission gp on gp.gperm_groupid=g.groupid
@@ -328,7 +328,7 @@ function showMenu($parentwindows_id,$level,$uid,$module_id){
 		where w.mid=$module_id and w.filename='$usefilename' and w.isdeleted=0
                 and gp.gperm_name='module_read' and u.uid=$uid
                 and ( gsp.validuntil = '0000-00-00' OR gsp.validuntil >= '$currentdate') order by gsp.iswriteperm DESC,gsp.permissionsetting DESC";
-      
+
 	$this->log->showLog(4, "With SQL: $sql");
 	$query=$this->xoopsDB->query($sql);
 
@@ -338,7 +338,7 @@ function showMenu($parentwindows_id,$level,$uid,$module_id){
 		return array($row['window_name'],$row['iswriteperm'],$row['windowsetting'],$row['permissionsetting'],$row['helpurl']);
 	else
  		return array("",0,0,"","");
-	
+
  }
 
 
@@ -350,14 +350,14 @@ function showMenu($parentwindows_id,$level,$uid,$module_id){
 		INNER JOIN sim_groups_users_link ug on o.groupid=ug.groupid
 		where ug.uid=$uid and o.isactive=1";
 
-	
+
 	$this->log->showLog(3,"Wtih SQL: $sql");
 	$wherestr="organization_id in(";
-			
+
 	$query=$this->xoopsDB->query($sql);
 	$i=0;
 	while($row=$this->xoopsDB->fetchArray($query)){
-		
+
 		$organization_id=$row['organization_id'];
 		if ($i==0)
 			$wherestr=$wherestr . $organization_id;
@@ -379,13 +379,13 @@ function showMenu($parentwindows_id,$level,$uid,$module_id){
 
 	if($includenull=='Y')
 		$selectctl=$selectctl . '<OPTION value="0" SELECTED="SELECTED">Null</OPTION>';
-		
+
 	$query=$this->xoopsDB->query($sql);
 	$selected="";
 	while($row=$this->xoopsDB->fetchArray($query)){
 		$uid=$row['uid'];
 		$uname=$row['uname'];
-	
+
 		if($id==$uid)
 			$selected='SELECTED="SELECTED"';
 		else
@@ -400,4 +400,3 @@ function showMenu($parentwindows_id,$level,$uid,$module_id){
    }
 }
 
-?>
